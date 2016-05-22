0adc142a97b2973f9104bda5a908f52fd2234687
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-05-22 19:50:12 +0200

    change DataSource

--- src/main/java/de/illilli/jdbc/ConnectionFactory.java
+++ src/main/java/de/illilli/jdbc/ConnectionFactory.java
@@ -34,11 +34,10 @@ import javax.sql.DataSource;
  */
 public class ConnectionFactory {
 
-	public static Connection getConnection() throws SQLException,
-			NamingException {
+	public static Connection getConnection() throws SQLException, NamingException {
 		Context initContext = new InitialContext();
 		Context envContext = (Context) initContext.lookup("java:/comp/env");
-		DataSource ds = (DataSource) envContext.lookup("jdbc/kvbraeder");
+		DataSource ds = (DataSource) envContext.lookup("jdbc/kvbrad");
 		Connection conn = ds.getConnection();
 		return conn;
 	}
